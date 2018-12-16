1a9cf96afd3a361581fdcc0875e7e648dd9691ba
Rodrigo Buhler <rodrigo.buhler@ymail.com>
2018-12-15 23:30:58 -0200

    Exercise 3 read claims

--- app/Day3_Fabric_Slice_Claims_v1.R
+++ app/Day3_Fabric_Slice_Claims_v1.R
@@ -4,41 +4,110 @@
 
 # -*-*-*-*-*-*-*-*-*-*-*-*
 
-count_duplicates<-function(id_list){
+read_claim<-function(claim){
   
-  list_size <- length(id_list)
-  str_result <- ""
-  count <- 0
+  seq  <- 0
+  left <- 0
+  top  <- 0
+  wide <- 0
+  tall <- 0
+  step <- ""
+  char_order <- ""
+  char_left  <- ""
+  char_top   <- ""
+  char_wide  <- ""
+  char_tall  <-""
+  coma <- FALSE
+  x <- FALSE
+  
+  numbers <- "1234567890"
   result <- vector()
   
-  for(i in 1:list_size){
-    id_rowA <- as.character(id_list[i])
-    id_size <- nchar(id_rowA)
+  claim_size <- nchar(claim)
+  
+  for(j in 1:claim_size){
+    char <- substr(claim,j,j)
+
+    if (step == "sequence"){
+      if (grepl(char, numbers, fixed=TRUE)){
+        char_order <- paste0(char_order, char)
+      }
+    }
     
-    for (j in 1:list_size){
-      id_rowB <- as.character(id_list[j])
-      
-      vec_rowA <- paste0(unlist(strsplit(id_rowA, "")))
-      vec_rowB <- paste0(unlist(strsplit(id_rowB, "")))
-      count <- id_size
-      
-      for (k in 1:id_size){
-        if (vec_rowA[k] == vec_rowB[k]){
-            str_result <- paste0( str_result, vec_rowA[k])
-            count <- count -1
+    if (step == "position"){
+      if (grepl(char, numbers, fixed=TRUE)){
+       
+        if (!coma){
+          char_left <- paste0(char_left, char)          
+        }else{
+          char_top <- paste0(char_top, char)  
+        }          
+      }else{
+        if (char == ','){
+          coma <- TRUE
         }
       }
-      
-      if ( count == 1 ) {
-        result[length(result)+1] <- str_result
-      }else{
-        count <- 0 
-        str_result <- ""
-      }
-      
     }
+
+    if (step == "dimention"){
+      if (grepl(char, numbers, fixed=TRUE)){
+        
+        if(!x){
+          char_wide <- paste0(char_wide, char)  
+        }else{
+          char_tall <- paste0(char_tall, char)  
+        }
+      }else{
+        if( char == 'x'){
+          x <- TRUE
+        }
+      }
+    }
+    
+    
+    # --------------------------------    
+    if (char == "#"){
+      step <- "sequence"
+    }
+    
+    if(char == "@"){
+      step <- "position"
+    }
+    
+    if(char == ":"){
+      step <- "dimention"
+    }    
+     
   }
-  result <- unique(result)
+  
+  order <- as.numeric(char_order)
+  left <- as.numeric(char_left)
+  top <- as.numeric(char_top)
+  wide <- as.numeric(char_wide)
+  tall <- as.numeric(char_tall)
+  
+  result <- c(order, left, top, wide, tall)
+  
+}
+
+count_overlap<-function(claim_list){
+  
+  return <- 0
+  # read claim list
+  list_size <- length(claim_list)
+  for (i in 1:list_size){
+    read <- read_claim(claim_list[i])
+    read
+    
+  }
+
+  # in an array of 1000 identify a claim
+  
+  # store each claim in a vector
+  
+  # return the vector of claims
+  
+  return
 }
 
 # # -*-*-*-*-*-*-*-*-*-*-*-*
@@ -54,16 +123,22 @@ source('app/library/Assert.R')
 
 # write.csv(payload, file = "app//payload//Day2_Inventory_Management_System.csv",row.names=FALSE, na="")
 
-variant <- c("abcde","fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz")
-expected <- "fgij"
-actual <- count_duplicates(variant)
-message  = '1'
+claim_one   = "#1 @ 1,3: 4x4"
+claim_two   = "#2 @ 3,1: 4x4"
+claim_three = "#3 @ 5,5: 2x2"
+
+claim_list = c(claim_one, claim_two, claim_three)
+
+variant <- claim_list
+expected <- 4
+actual <- count_overlap(variant)
+message  = 'Variant 1'
 myAssert.integer.equals(message, expected, actual[1])
 
 # -------- GET THE RESULT AFTER TESTING 
 
-payload <- read.csv(file = "app//payload//Day2_Inventory_Management_System2.csv",header=FALSE)$V1
-result <- count_duplicates(payload)
-
-print("--- Result ---")
-print(result)
+# payload <- read.csv(file = "app//payload//Day2_Inventory_Management_System2.csv",header=FALSE)$V1
+# result <- count_overlap(payload)
+# 
+# print("--- Result ---")
+# print(result)
